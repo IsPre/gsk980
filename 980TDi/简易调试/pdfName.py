48d43f5356a9f528b7dcb0be16aee9537f1f30f0
email <lyyuoo@hotmail.com>
2024-04-02 13:44:41 +0800

    980TDi增加接口说明目录

---
+++ 980TDi/简易调试/pdfName.py
@@ -0,0 +1,33 @@
+import os
+import re
+import json
+from urllib.parse import quote
+
+# 指定目录路径
+directory = "H:\\Desktop\\gitee\\lyyuoo\\980TDi\\简易调试"
+
+# 获取目录下所有文件
+files = os.listdir(directory)
+
+# 提取所有PDF文件名
+pdf_files = [file for file in files if file.endswith(".pdf")]
+
+# 按照数字序号排序PDF文件名
+pdf_files.sort(
+    key=lambda x: [int(s) if s.isdigit() else s.lower() for s in re.split(r"(\d+)", x)]
+)
+
+# 构建输出格式
+output_data = {"content_1": []}
+for pdf_file in pdf_files:
+    name = os.path.splitext(pdf_file)[0]  # 去掉.pdf后缀
+    url = f"https://gitee.com/lyyuoo/lyyuoo/raw/master/980TDi/%E7%AE%80%E6%98%93%E8%B0%83%E8%AF%95/{quote(pdf_file)}"
+
+    output_data["content_1"].append({"name": name, "url": url})
+
+# 保存为JSON格式
+output_file = "H:\\Desktop\\gitee\\lyyuoo\\980TDi\\简易调试/catalogue2.json"
+with open(output_file, "w", encoding="utf-8") as f:
+    json.dump(output_data, f, ensure_ascii=False, indent=4)
+
+print(f"PDF文件名已按数字序号排序并保存为JSON格式到 {output_file} 文件中。")
