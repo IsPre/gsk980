4d09070d8723e25c2931b61d170e2d055affea4f
email <lyyuoo@hotmail.com>
2024-04-02 15:05:57 +0800

    更新文档路径生成文件

--- 980TDi/新功能/pdfName.py
+++ 980TDi/新功能/pdfName.py
@@ -16,17 +16,17 @@ pdf_files = [file for file in files if file.endswith(".pdf")]
 pdf_files.sort(
     key=lambda x: [int(s) if s.isdigit() else s.lower() for s in re.split(r"(\d+)", x)]
 )
-
+parentDir = "新功能"
 # 构建输出格式
 output_data = {"content_1": []}
 for pdf_file in pdf_files:
     name = os.path.splitext(pdf_file)[0]  # 去掉.pdf后缀
-    url = f"https://gitee.com/lyyuoo/lyyuoo/raw/master/980TDi/%E6%96%B0%E5%8A%9F%E8%83%BD/{quote(pdf_file)}"
+    url = f"https://gitee.com/lyyuoo/lyyuoo/raw/master/980TDi/{quote(parentDir)}/{quote(pdf_file)}"
 
     output_data["content_1"].append({"name": name, "url": url})
 
 # 保存为JSON格式
-output_file = "H:\\Desktop\\gitee\\lyyuoo\\980TDi\\新功能/catalogue1.json"
+output_file = f"H:\\Desktop\\gitee\\lyyuoo\\980TDi\\{parentDir}/catalogue1.json"
 with open(output_file, "w", encoding="utf-8") as f:
     json.dump(output_data, f, ensure_ascii=False, indent=4)
 
